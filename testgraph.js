efbd834d8d481a7b6fca63545aa14d1a733a448b
Eric Heinzman <ehu9079@ehu9079-VirtualBox.(none)>
2015-03-17 22:12:48 -0400

    testgraph for homework. Includes 10 vertices

--- testgraph.js
+++ testgraph.js
@@ -1,8 +1,24 @@
 
 load("Graph.js");
-g = new Graph(5);
-g.addEdge(0,1);
-g.addEdge(0,2);
+g = new Graph(10);
 g.addEdge(1,3);
+g.addEdge(2,3);
+g.addEdge(2,5);
+g.addEdge(2,6);
+g.addEdge(3,4);
+g.addEdge(4,7);
+g.addEdge(5,8);
+g.addEdge(6,10);
+g.addEdge(7,10);
+g.addEdge(8,9);
+g.addEdge(9,10);
 g.addEdge(2,4);
+
+
 g.showGraph();
+print('Test');
+var vertex = 10;
+var source = 2;
+var paths = g.pathTo(source,vertex);
+g.showPath(paths);
+
